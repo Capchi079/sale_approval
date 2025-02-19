a8f799a7799c74babf7c8a252e66e43124558d90
Capchi079 <satyajitbehera.chiku079@gmail.com>
2025-02-19 14:49:37 +0530

    my code latest

--- sale_work_custom/models/sale_inherit.py
+++ sale_work_custom/models/sale_inherit.py
@@ -18,14 +18,23 @@ class SaleOrderInherit(models.Model):
 
     def action_second_approval(self):
         for record in self:
-            if record.state == 'approval_one':  # Check if current state is 'draft'
-                record.state = 'approval_two'  # Set the state to 'pre_ongoing'
+            if record.state == 'approval_one':
+                record.state = 'approval_two'
             else:
-                raise UserError("You can only move to 'Pre-Ongoing' from 'Draft' state.")
+                raise UserError("You can only move to from 'Draft' state.")
 
     @api.model
     def create(self, vals):
-        """Override the create method to set default state to 'approval_one'."""
-        vals['state'] = 'approval_one'  # Automatically set state to 'approval_one'
+
+        vals['state'] = 'approval_one'
         return super(SaleOrderInherit, self).create(vals)
+
+    def action_custom_confirm(self):
+        """ Custom Confirm Button - Moves from approval_two to sale """
+        for record in self:
+            if record.state == 'approval_two':
+                record.state = 'sale'
+            else:
+                raise UserError("You Need Approval From The CEO.")
+
 
